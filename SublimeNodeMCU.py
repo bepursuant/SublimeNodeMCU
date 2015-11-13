998ddf9ef873c6b3526c3913ef1698f937c5ff5e
Kevin <kklika@pursuantsolutions.com>
2015-11-12 23:19:22 -0600

    Passthrough after Run
    Added feature when file is run after upload, the output is shown in the
    python console. This makes it really easy to test small scripts by
    uploading and immediately seeing the console output.
    Various other updates to style, order of operations, pulled some things
    into functions...
    Refactored WaitForPrompt and WaitForAck into a similar WaitFor function.

--- SublimeNodeMCU.py
+++ SublimeNodeMCU.py
@@ -1,56 +1,89 @@
 import os, serial, sys
 
-verbose = True
+# These settings will control how the script interacts with your NodeMCU. The defaults
+# are sensible, but you will likely need to adjust them for your specific computer.
+# 
+port = 'COM4'   #the COM port to which the NodeMCU is connected
+baud = 9600     #usually 9600 or 115200
 
-#Serial Configuration
-port = 'COM4'
-baud = 9600 #usually 9600 or 115200
+reset = True    #automtically reset the NodeMCU before writing
+execute = True  #automatically execute uploaded file after upload
 
-reset = True #automtically reset the NodeMCU before writing
-execute = False #automatically execute uploaded file after upload
+verbose = True  #write debugging information to the console
 
+prompt = b'\n> '#this is the prompt we will wait for at times
 
+ack = b'Y'      #this is the ack character from the receive script
 
-#########################################################
-#define a lua script to receive the data we will be sending
-receiveLua = \
-r"""file.remove('%s')
-    file.open('%s', 'w')
-    uart.on('data', 255,
-      function (d)
-        c = tonumber(d:sub(1, 4))
-        d = d:sub(5, 4+c)
-        file.write(d)
-        if c ~= 251 then
-            file.close()
-        end
-        uart.write(0, 'Y')
-      end, 0)"""
-receiveLua = ' '.join([line.strip() for line in receiveLua.split('\n')]).replace(', ', ',')
+######################################################################################################################
 
-prompt = b'\n> '
-def waitForPrompt():
-    data = b''
-    while data[-3:] != prompt:
+filepath = sys.argv[1]
+filename = os.path.basename(filepath)
+
+# This helper function will send to the NodeMCU a script that will be immediately
+# run. This script waits for data on the uart, and writes it to the file. This
+# is much faster than the ESPlorer method of sending the code line by line.
+def sendReceiveScript(serial, filename='init.lua'):
+    receiveLua = \
+    r"""file.remove('%s')
+        file.open('%s', 'w')
+        uart.on('data', 255,
+          function (d)
+            c = tonumber(d:sub(1, 4))
+            d = d:sub(5, 4+c)
+            file.write(d)
+            if c ~= 251 then
+                file.close()
+                uart.write(0, 'Y')
+                return
+            end
+            uart.write(0, 'Y')
+          end, 0)"""
+    receiveLua = ' '.join([line.strip() for line in receiveLua.split('\n')]).replace(', ', ',')
+
+    save_command = receiveLua % (filename, filename) + '\r'
+
+    assert len(save_command) < 256, 'Save_command too long: %s bytes' % len(save_command)
+
+    writeV('Sending receive script to NodeMCU...')
+    serial.write(save_command.encode())
+    response = waitFor(prompt)
+    #assert response == save_command + prompt, response
+    writeLnV('ok!')
+
+# Because we are interacting with the NodeMCU through the console, we will at times
+# need to wait for it to finish processing before we send our next command. This
+# function can also passthrough the serial data between the console and COMs.
+def waitFor(chars, passthrough=False):
+    charslen = len(chars)
+
+    data = b''          #to hold the serial data we receive
+
+    #writeLnV('Waiting for: ' + str(chars) + ' len:' + str(-charslen))
+
+    # While the last three bytes of the data variable are not equal to the chars
+    # we are waiting for, we read a byte from the serial port and append it to
+    # the data variable. This is also where we passthrough the serial data.
+    while data[-charslen:] != chars:
         d = ser.read()
-        #writeV(d.decode())
 
-        if d == b'':
-            writeV('.')
+        if(passthrough):
+            write(d.decode())
+
+        #if d == b'':    #writing a dot to console every time we get a blank byte
+        #    writeV('.')
 
         data += d
 
     return data
 
-def waitForAck():
-    d = b''
-    while True:
-        d = ser.read(1)
-        #writeLnV("ACK?" + d.decode())
-
-        if(d == b'Y'):
-            break
-    return
+def resetNodeMCU():
+    writeV('Reset...')
+    ser.setDTR(False) # Setting them to False sets their logic level back to 1.
+    ser.setRTS(False)
+    waitFor(prompt) # Wait until Lua has booted after the reset caused by RTS.
+                    # This waits forever in case RTS is not connected to RESET.
+    writeLnV('ok!')
 
 def write(towrite):
     sys.stdout.write(towrite)
@@ -67,44 +100,40 @@ def writeLnV(towrite):
     if(verbose):
         writeLn(towrite);
 
-writeLn('Uploading to NodeMCU...')
+# Here is where we actually begin our script! Here we will open our serial port, and begin
+# interacting with the NodeMCU over it.
+#
+writeLn('[SublimeNodeMCU] Uploading...');
 
 writeV('Opening serial port ' + port + ':' + str(baud) + '...')
-ser = serial.Serial(port, baud, timeout=1)
+
+try:
+    ser = serial.Serial(port, baud, timeout=1)
+except serial.SerialException as e:
+    writeLnV('failed!')
+    sys.exit()
+
 writeLnV('ok!')
 
 # Opening the serial port changes DTR and RTS to 0.
-
 if reset:
-    writeV('Reset')
-    ser.setDTR(False) # Setting them to False sets their logic level back to 1.
-    ser.setRTS(False)
-    waitForPrompt() # Wait until Lua has booted after the reset caused by RTS.
-                    # This waits forever in case RTS is not connected to RESET.
-    writeLnV('ok!')
+    resetNodeMCU()
 else:
-    writeLn('If nothing happens, try setting reset = True in uploader.py')
+    writeLn('If nothing happens, try setting reset = True in SublimeNodeMCU.py')
 
-file_path = sys.argv[1]
-file_name = os.path.basename(file_path)
-save_command = receiveLua % (file_name, file_name) + '\r'
 
-assert len(save_command) < 256, 'Save_command too long: %s bytes' % len(save_command)
+sendReceiveScript(ser, filename);   #send receive script to NodeMCU to get ready to receive data
 
-#let's send the save command to the nodeMCU to get it ready to receive the data
-writeV('Sending Save Command to NodeMCU...')
-ser.write(save_command.encode())
-response = waitForPrompt()
-#assert response == save_command + prompt, response
-writeLnV('ok!')
 
 #read the file into buffer
-writeV('Reading ' + file_path + ' into memory...')
-f = open( file_path, 'rb' ); content = f.read(); f.close()
+writeV('Reading ' + filepath + ' into memory...')
+f = open( filepath, 'rb' ); content = f.read(); f.close()
 writeLnV('ok!')
 
 pos = 0
 chunk_size = 251 # 255 (maximum) - 4 (hex_size)
+
+writeV('Sending...')
 
 while pos <= len(content):
     data = content[pos : pos + chunk_size]
@@ -119,17 +148,26 @@ while pos <= len(content):
     #writeLnV("Writing: " + str(hex_size.encode()) + str(data))
     ser.write(hex_size.encode() + data)
 
-    waitForAck()
+    waitFor(ack)
 
     percent = int(100 * pos / ((len(content) + chunk_size) / chunk_size * chunk_size))
 
     write('%3s %%' % percent)
 
+if percent != 100:  #literally just so it looks pretty
+    write(' 100 %')
+
 #we're done!
 writeLn('')
+writeLn('ok!')
 
-if execute and file_name.endswith('.lua'):
-    ser.write('dofile("%s")\r' % file_name)
-    waitForPrompt(do_log=True)
+if execute and filename.endswith('.lua'):
+    if(reset):
+        resetNodeMCU()
+
+    writeV('Running ' + str(filename) + '... > ')
+    run_command = 'dofile("%s")\r' % filename
+    ser.write(run_command.encode())
+    waitFor(chars=prompt, passthrough=True)
 
 ser.close()
